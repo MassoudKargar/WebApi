a847edc6469d72280cfe7f9cfe7cb268b4d74b03
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2021-08-01 11:14:20 -0700

    Create Library

---
+++ Src/Api.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Api.Entities
+{
+    public class Class1
+    {
+    }
+}
---
+++ Src/Api.WebFramework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Api.WebFramework
+{
+    public class Class1
+    {
+    }
+}
