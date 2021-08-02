5b69232b1e2e9b7aeebea327c059feb5ebbeac29
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2021-08-02 09:34:09 -0700

    Create Repositories In Data

---
+++ Src/Api.Data/Contracts/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Api.Entities;
+
+namespace Api.Data.Contracts
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user, string password, CancellationToken cancellationToken);
+        Task<User> GetByUserAndPass(string username, string password, CancellationToken cancellationToken);
+        Task UpdateLastLoginDateAsync(User user, CancellationToken cancellationToken);
+        Task UpdateSecurityStampAsync(User user, CancellationToken cancellationToken);
+    }
+}
